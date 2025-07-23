985813594c32cd30b8ad7147f9b5a1a2ee7a1b75
shreyanalluri <nallshrey@gmail.com>
2025-07-23 09:22:26 -0400

    - Created pm25_components__washu.jsonl - Added temporal/spatial coverage metadata to subdatasets - Updated ReD paths with "lab_share" - Updated DOI for environmental datasets - Made github links visible - rendered site

--- create_catalog.sh
+++ create_catalog.sh
@@ -37,6 +37,9 @@ datalad catalog-add --catalog lego --metadata json/pm25__ushap.jsonl
 datalad catalog-validate --metadata json/pm25__washu.jsonl
 datalad catalog-add --catalog lego --metadata json/pm25__washu.jsonl
 
+datalad catalog-validate --metadata json/pm25_components__washu.jsonl
+datalad catalog-add --catalog lego --metadata json/pm25_components__washu.jsonl
+
 datalad catalog-validate --metadata json/climate_types__koppen_geiger.jsonl
 datalad catalog-add --catalog lego --metadata json/climate_types__koppen_geiger.jsonl
 
