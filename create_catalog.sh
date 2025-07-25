7b31524b8ca16e98f8cb00fbabaffa5a848fbe52
audiracmichelle <audiracmichelle@gmail.com>
2025-07-24 23:14:09 -0400

    modified tab tags

--- create_catalog.sh
+++ create_catalog.sh
@@ -4,6 +4,9 @@ datalad catalog-create --catalog lego --config-file config.json
 cp app_component_dataset.js lego/assets/app_component_dataset.js
 cp dataset-template.html lego/templates/dataset-template.html
 cp index.html lego/index.html
+cp favicon.ico lego/assets/favicon/favicon.ico
+cp favicon-16x16.png lego/assets/favicon/favicon-16x16.png
+cp favicon-32x32.png lego/assets/favicon/favicon-32x32.png
 
 datalad catalog-validate --metadata json/lego.jsonl
 datalad catalog-add --catalog lego --metadata json/lego.jsonl
